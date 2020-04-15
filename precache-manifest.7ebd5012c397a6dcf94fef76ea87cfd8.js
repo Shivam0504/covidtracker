71bbe5283215aad81383c5010e81cb398ad90571
Shivam0504 <shivamkirti71@gmail.com>
2020-04-15 19:07:19 +0530

    Updates

---
+++ precache-manifest.7ebd5012c397a6dcf94fef76ea87cfd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe7ee37ffb19bfb02c4be394ceccdbb",
+    "url": "/covidtraker/index.html"
+  },
+  {
+    "revision": "839a3ef8b2647a4fdbdf",
+    "url": "/covidtraker/static/js/2.f3e7c9fe.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covidtraker/static/js/2.f3e7c9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13088520a6aabc2b372f",
+    "url": "/covidtraker/static/js/main.0da06924.chunk.js"
+  },
+  {
+    "revision": "d0f56344a0cc08a3abb7",
+    "url": "/covidtraker/static/js/runtime-main.01576fc0.js"
+  }
+]);
